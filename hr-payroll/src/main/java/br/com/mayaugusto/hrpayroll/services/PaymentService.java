956dcf4d9e07c4e48ced5e6bebce5a9463bb920a
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-04-24 10:35:32 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/mayaugusto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/mayaugusto/hrpayroll/services/PaymentService.java
@@ -1,12 +1,33 @@
 package br.com.mayaugusto.hrpayroll.services;
 
 import br.com.mayaugusto.hrpayroll.entities.Payment;
+import br.com.mayaugusto.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriAttributes = new HashMap<>();
+        uriAttributes.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriAttributes);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
