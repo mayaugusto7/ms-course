5a1718ed7793149df33c0f7cc2ef9d37524342b8
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-04-30 11:44:40 -0300

    Implementing Ribbon Load Balancing

--- hr-payroll/src/main/java/br/com/mayaugusto/hrpayroll/foreignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/com/mayaugusto/hrpayroll/foreignclients/WorkerFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "workers")
+@FeignClient(name = "hr-worker", path = "workers")
 public interface WorkerFeignClient {
     @GetMapping( value = "/{id}")
     ResponseEntity<Worker> findById(@PathVariable Long id);
