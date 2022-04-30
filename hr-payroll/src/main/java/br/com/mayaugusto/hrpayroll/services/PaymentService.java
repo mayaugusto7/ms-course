3aad4e6275f4ea24264eda5753f6848b3754880a
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-04-30 11:18:19 -0300

    Feign Client

--- hr-payroll/src/main/java/br/com/mayaugusto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/mayaugusto/hrpayroll/services/PaymentService.java
@@ -2,32 +2,20 @@ package br.com.mayaugusto.hrpayroll.services;
 
 import br.com.mayaugusto.hrpayroll.entities.Payment;
 import br.com.mayaugusto.hrpayroll.entities.Worker;
+import br.com.mayaugusto.hrpayroll.foreignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
-
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClient workerFeignClient;
 
     @Autowired
-    public PaymentService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public PaymentService(WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
-
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriAttributes = new HashMap<>();
-        uriAttributes.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriAttributes);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
