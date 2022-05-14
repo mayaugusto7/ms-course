7589236554531dbac63799c22f7383e4daf278a2
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-05-14 18:40:38 -0300

    hr-worker config server client

--- hr-worker/src/main/java/br/com/mayaugusto/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/mayaugusto/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import br.com.mayaugusto.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,8 @@ import java.util.List;
 public class WorkerResource {
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
     private final Environment env;
 
     private final WorkerRepository workerRepository;
@@ -27,6 +30,12 @@ public class WorkerResource {
     public WorkerResource(Environment env, WorkerRepository workerRepository) {
         this.env = env;
         this.workerRepository = workerRepository;
+    }
+
+    @GetMapping(value = "configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
